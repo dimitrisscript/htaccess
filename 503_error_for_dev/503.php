579b6c91eb0d8be374d06836622a5bc6ec2689c2
Dimitris Siakavelis <dimitrisscript@gmail.com>
2016-11-01 16:47:43 +0200

    503 throw dev error
    This will throw a 503 to all except a selected IP. It is used for hiding
    the under-development websites from the public, while developing them
    from the selected IP address.

---
+++ 503_error_for_dev/503.php
@@ -0,0 +1,23 @@
+<?php
+
+header('HTTP/1.1 503 Service Temporarily Unavailable');
+header('Status: 503 Service Temporarily Unavailable');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta name="description" content="This is our 503 page, something big is coming! :)">
+	<title>503 Website.com</title>
+</head>
+<body>
+	<h6>Error 503</h6>
+	<h1>Please wait!</h1>
+	<p>We are currently building this website!</p>
+	<p>We should be up in a couple of weeks.</p>
+</body>
+</html>
